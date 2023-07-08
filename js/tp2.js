a010240e7a5187443cc1815d4c42a1e72b28b8c7
Julieta Parra <parrajuliet@hotmail.com>
2023-07-08 07:05:47 -0300

    fin tp2

--- js/tp2.js
+++ js/tp2.js
@@ -5,3 +5,28 @@ const productos = [{id: 1, nombre: "Alfajores de Nuez", precio: 550, stock: 48},
 ]
 console.table(productos)
 
+
+const alfajorcitos = ["Alfajorcitos de nuez", "Alfajorcitos de almendra", "Alfajorcitos de chocolate", "Alfajorcitos de coco"]
+
+const galletitas = ["Galletitas de limón rellenas", "Galletitas de café", "Melcochudas", "Lunettes"]
+
+const tartas = ["Lemon pie", "Tarta de manzana", "Tarta de pera", "Tarta de ricots"]
+
+const pasteleria = alfajorcitos.concat(galletitas.concat(tartas))
+
+function agregarGalletitas () {
+    //"Galletitas de vainilla", "Galletitas de coco"
+    let nuevaGalletita = prompt("Ingresa una nueva galletita al menú: ")
+    if (nuevaGalletita.trim !== ""){  
+        galletitas.push(nuevaGalletita)
+        console.clear 
+        console.table(galletitas) 
+    }
+}
+
+function quitarGalletitas () {
+    console.clear()
+    let galletitaQuitada = galletitas.pop() 
+    alert("Se ha quitado una galletita del menú" + galletitaQuitada)
+    console.table(galletitas)
+} 
